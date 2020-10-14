8c8c9dd9d0351d327b6e47715069f0ece9a9736d
Lan1018 <frankielan168@gmail.com>
2020-10-14 19:02:34 +0800

    Updates

---
+++ precache-manifest.c2ea7438a325e674189e03d4d39fc3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e926059eaad32b080f300bd24ee7e15",
+    "url": "/1091-208410364-robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-208410364-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "29a283fc6312ff0f7040",
+    "url": "/1091-208410364-robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-208410364-robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-208410364-robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a283fc6312ff0f7040",
+    "url": "/1091-208410364-robofriends/static/js/main.7ac31b10.chunk.js"
+  },
+  {
+    "revision": "087c249d028787d698ca",
+    "url": "/1091-208410364-robofriends/static/js/runtime-main.bc393ea3.js"
+  }
+]);
